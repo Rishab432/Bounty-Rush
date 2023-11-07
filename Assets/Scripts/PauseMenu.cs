436b84168cfe47c3e043426de389eab656e4eca2
Rishab Areekkal <rishab.areekkal24@ycdsbk12.ca>
2023-11-07 18:11:51 -0500

    made the pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,57 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public static PauseMenu Instance;
-    public GameObject pauseMenu;
-    public GameObject pauseButton;
+    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _pauseButton;
     public static bool isPaused;
 
-    void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        pauseMenu.SetActive(false);
+        _pauseMenu.SetActive(false);
     }
 
     public void PauseGame()
     {
-        pauseMenu.SetActive(true);
-        pauseButton.SetActive(false);
+        _pauseMenu.SetActive(true);
+        _pauseButton.SetActive(false);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
     public void ResumeGame()
     {
-        pauseMenu.SetActive(false);
-        pauseButton.SetActive(true);
+
+        _pauseMenu.SetActive(false);
+        _pauseButton.SetActive(true);
         Time.timeScale = 1f;
         isPaused = false;
     }
 
-    public void toMainMenu()
+    public void ToRestart()
     {
-        pauseMenu.SetActive(false);
-        pauseButton.SetActive(true);
+        _pauseMenu.SetActive(false);
+        _pauseButton.SetActive(true);
         Time.timeScale = 1f;
         isPaused = false;
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("playscene");
+    }
+
+    public void ToMainMenu()
+    {
+        _pauseMenu.SetActive(false);
+        _pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene("mainmenuscene");
     }
 
     public void QuitGame()
