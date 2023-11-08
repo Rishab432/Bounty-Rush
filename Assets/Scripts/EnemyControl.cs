746f05e369f097f717ddca2515c4ec69e7c51a16
alanjsuratos <125233948+alanjsuratos@users.noreply.github.com>
2023-11-08 03:56:57 -0500

    Enemy spawn, enemy movement

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    float speedx;
+    float speedy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedx = 2f;
+        speedy = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+
+        position = new Vector2(position.x - speedx * Time.deltaTime, position.y - speedy * Time.deltaTime);
+
+        transform.position = position;
+
+      //  Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        if (transform.position.x < -4.4)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
